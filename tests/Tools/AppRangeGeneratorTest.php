5d42442d74659dc9e16f7330bec11eaaa498f158
Carlos <code_dev@zoho.com>
2020-06-08 06:35:45 -0400

    creacion de lector de archivos

--- tests/Tools/AppRangeGeneratorTest.php
+++ tests/Tools/AppRangeGeneratorTest.php
@@ -6,26 +6,54 @@ namespace App\Tests\Tools;
 
 use PHPUnit\Framework\TestCase;
 use App\Tools\AppRangeGenerator;
+use App\Tools\AppOfficeFactory;
 
 class AppRangeGeneratorTest extends TestCase
 {
 
-    public function testRange()
+    public function testRange(): void
     {
-        $array = ['A','AA'];
+        $array = ['AL','AZ'];
         $run_func = new AppRangeGenerator();
         $result_func = $run_func->makeRange($array);
-        //var_dump($result_func);
+        var_dump($result_func);
         $this->assertIsArray($result_func);
     }
 
-    public function testInverseRange()
+    public function testInverseRange(): void
     {
         //fail
-        $array = ['x','A'];
+        $array = ['X','A'];
         $run_func = new AppRangeGenerator();
         $result_func = $run_func->makeRange($array);
         //var_dump($result_func);
         $this->assertNull($result_func);
+    }
+
+    public function testIterador(): void //falla al no iniciar en el valor estipulado
+    {
+        $memA = AppRangeGenerator::stringToArray('AL');
+        $memB = AppRangeGenerator::stringToArray('AZ');
+        $array = [$memA,$memB];
+        $result_func = AppRangeGenerator::iterador($array);
+        var_dump($result_func);
+        $this->assertIsArray($result_func);
+    }
+
+    public function testOfficeFactory(): void
+    {
+        $array = [
+            __DIR__ . '/../../var/temp/test.txt',
+            "txt",
+            ["A","D"],
+            ["X","AA"],
+            ["AL","AX"]//genera un error de inicio
+        ];
+
+        $run_func = new AppOfficeFactory();
+        $result_func = $run_func->multiRangeFactory($array);
+        var_dump($result_func);
+        //$this->assertNull($result_func);
+        $this->assertIsArray($result_func);
     }
 }
